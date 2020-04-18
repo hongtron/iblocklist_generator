d28872de70d025382f67bad19042ec4fabd6819b
ali <github@alihong.net>
2020-04-18 11:45:05 -0500

    clean it up

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 use std::collections::HashMap;
-use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::Write;
-use tempfile::tempfile;
+use tempfile::NamedTempFile;
 use flate2::read::GzDecoder;
-use std::fmt::Error;
-use std::path::Path;
 
 fn blocklist_uri(id: &str) -> String {
     format!("https://list.iblocklist.com/?list={}&fileformat=p2p&archiveformat=gz", id)
 }
 
 fn get_blocklist(id: &str) -> Result<File, io::Error> {
-    let pathstr = format!("/tmp/{}.gz", id);
-    let path = Path::new(&pathstr);
-    // fs::create_dir_all(path).unwrap();
-    // let mut dest = File::create(&format!("/tmp/{}.gz", id)).unwrap();
-    // let mut dest = tempfile()?;
-    let resource = format!("https://list.iblocklist.com/?list={}&fileformat=p2p&archiveformat=gz", id);
+    let resource = blocklist_uri(id);
     let body = reqwest::blocking::get(&resource).unwrap().bytes().unwrap();
-    let mut dest= File::create(path).unwrap();
-    println!("{:?}", dest);
-    dest.write_all(&body);
-    let reader = File::open(path)?;
+    let mut dest = NamedTempFile::new()?;
+    dest.write_all(&body)?;
+    let reader = dest.reopen()?;
     Ok(reader)
 }
 
@@ -40,12 +31,9 @@ fn main() -> Result<(), io::Error> {
     }).collect();
 
     for (list_name, f) in local_blocklists {
-        println!("file for gz {:?}", f);
         let mut gz = GzDecoder::new(f);
-        let pathstr = format!("/tmp/{}.txt", list_name);
-        let path = Path::new(&pathstr);
-        let mut file = File::create(path).unwrap();
-        io::copy(&mut gz, &mut file).unwrap();
+        let mut list_out = File::create(format!("/tmp/{}.txt", list_name)).unwrap();
+        io::copy(&mut gz, &mut list_out).unwrap();
     };
 
     Ok(())
