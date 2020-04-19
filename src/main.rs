fac538430d243817deb13f36fe9a984d698de832
ali <github@alihong.net>
2020-04-19 10:47:23 -0500

    generate combined list

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
-use std::io::Write;
+use std::io::{Write, BufReader, BufRead};
 use tempfile::NamedTempFile;
 use flate2::read::GzDecoder;
 use anyhow::Result;
+use regex::Regex;
 
 fn blocklist_uri(id: &str) -> String {
     format!("https://list.iblocklist.com/?list={}&fileformat=p2p&archiveformat=gz", id)
@@ -26,16 +27,32 @@ fn main() -> Result<()> {
         ("level3", "uwnukjqktoggdknzrhgh"),
     ].iter().cloned().collect();
 
-    let local_blocklists: Vec<(&&str, File)> = blocklists.iter().map(|(list_name, id)| {
+    let local_blocklists: Vec<File> = blocklists.iter().map(|(list_name, id)| {
         println!("Downloading I-Blocklist {}...", list_name);
-        (list_name, get_blocklist(&id).unwrap())
+        get_blocklist(&id).unwrap()
     }).collect();
 
-    for (list_name, f) in local_blocklists {
+    let decompressed_blocklists: Vec<Result<File>> = local_blocklists.iter().map(|f| {
         let mut gz = GzDecoder::new(f);
-        let mut list_out = File::create(format!("/tmp/{}.txt", list_name))?;
+        let mut list_out = NamedTempFile::new()?;
         io::copy(&mut gz, &mut list_out).unwrap();
-    };
+        let reader = list_out.reopen()?;
+        Ok(reader)
+    }).collect();
+
+    let mut combined_list = File::create("blocklist.txt")?;
+    let empty_line_or_comment: Regex = Regex::new(r"(^$|^#.*$)").unwrap();
+
+    for f in decompressed_blocklists {
+        let f = f.unwrap();
+        let r = BufReader::new(f);
+        let valid_lines = r.lines()
+            .map(|l| l.unwrap())
+            .filter(|line| !empty_line_or_comment.is_match(line));
+        for line in valid_lines {
+            writeln!(combined_list, "{}", line)?;
+        }
+    }
 
     Ok(())
 }
