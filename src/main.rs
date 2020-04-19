20296987d6256ca599e78cdfc5080eb8bdada2ba
ali <github@alihong.net>
2020-04-19 11:52:43 -0500

    refactor, create blocklist struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::{Write, BufReader, BufRead};
@@ -7,12 +6,18 @@ use flate2::read::GzDecoder;
 use anyhow::Result;
 use regex::Regex;
 
+struct Blocklist<'a> {
+    id: &'a str,
+    name: &'a str,
+}
+
 fn blocklist_uri(id: &str) -> String {
     format!("https://list.iblocklist.com/?list={}&fileformat=p2p&archiveformat=gz", id)
 }
 
-fn get_blocklist(id: &str) -> Result<File> {
-    let resource = blocklist_uri(id);
+fn download_list(blocklist: &Blocklist) -> Result<File> {
+    println!("Downloading list: {}", blocklist.name);
+    let resource = blocklist_uri(blocklist.id);
     let body = reqwest::blocking::get(&resource)?.bytes()?;
     let mut dest = NamedTempFile::new()?;
     dest.write_all(&body)?;
@@ -20,35 +25,41 @@ fn get_blocklist(id: &str) -> Result<File> {
     Ok(reader)
 }
 
+fn decompress_list(f: File) -> Result<File> {
+    let mut gz = GzDecoder::new(f);
+    let mut list_out = NamedTempFile::new()?;
+    io::copy(&mut gz, &mut list_out).unwrap();
+    let reader = list_out.reopen()?;
+    Ok(reader)
+}
+
+fn valid_lines(f: File) -> Vec<String> {
+    let empty_line_or_comment: Regex = Regex::new(r"(^$|^#.*$)").unwrap();
+    let r = BufReader::new(f);
+    let valid_lines = r.lines()
+        .map(|l| l.unwrap())
+        .filter(|line| !empty_line_or_comment.is_match(line))
+        .collect();
+    valid_lines
+}
+
 fn main() -> Result<()> {
-    let blocklists: HashMap<&str, &str> = [
-        ("level1", "ydxerpxkpcfqjaybcssw"),
-        ("level2", "gyisgnzbhppbvsphucsw"),
-        ("level3", "uwnukjqktoggdknzrhgh"),
-    ].iter().cloned().collect();
+    let blocklists = vec![
+        Blocklist { name: "level1", id: "ydxerpxkpcfqjaybcssw" },
+        Blocklist { name: "level2", id: "gyisgnzbhppbvsphucsw" },
+        Blocklist { name: "level3", id: "uwnukjqktoggdknzrhgh" },
+    ];
 
-    let local_blocklists: Vec<File> = blocklists.iter().map(|(list_name, id)| {
-        println!("Downloading I-Blocklist {}...", list_name);
-        get_blocklist(&id).unwrap()
-    }).collect();
-
-    let decompressed_blocklists: Vec<Result<File>> = local_blocklists.iter().map(|f| {
-        let mut gz = GzDecoder::new(f);
-        let mut list_out = NamedTempFile::new()?;
-        io::copy(&mut gz, &mut list_out).unwrap();
-        let reader = list_out.reopen()?;
-        Ok(reader)
-    }).collect();
+    let local_blocklists: Vec<Result<File>> = blocklists.iter()
+        .map(|blocklist| { download_list(&blocklist).unwrap() })
+        .map(|f| decompress_list(f))
+        .collect();
 
     let mut combined_list = File::create("blocklist.txt")?;
-    let empty_line_or_comment: Regex = Regex::new(r"(^$|^#.*$)").unwrap();
 
-    for f in decompressed_blocklists {
+    for f in local_blocklists {
         let f = f.unwrap();
-        let r = BufReader::new(f);
-        let valid_lines = r.lines()
-            .map(|l| l.unwrap())
-            .filter(|line| !empty_line_or_comment.is_match(line));
+        let valid_lines = valid_lines(f);
         for line in valid_lines {
             writeln!(combined_list, "{}", line)?;
         }
