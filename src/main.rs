a8d368117bdb79a6a4d40b62a9e7e43fd96695e3
ali <github@alihong.net>
2021-02-21 14:31:06 -0600

    learning about async

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use std::fs::File;
-use std::io;
-use std::path::PathBuf;
-use std::io::{Write, BufReader, BufRead};
-
-use tempfile::NamedTempFile;
-use flate2::read::GzDecoder;
-use anyhow::Result;
+use std::fmt::Write;
+use std::sync::Arc;
+use async_std::sync::Mutex;
+use async_std::task;
+use flate2::bufread::GzDecoder;
 use regex::Regex;
-use structopt::StructOpt;
-
-
-#[derive(Debug, StructOpt)]
-#[structopt(name = "iblocklist_generator", about = "Fetch and combine lists from iblocklist.com")]
-struct Opt {
-    /// Combined list destination; writes to stdout if not specified.
-    #[structopt(short, long, parse(from_os_str))]
-    output_file: Option<PathBuf>,
-}
+use std::io::Read;
 
 struct Blocklist<'a> {
     id: &'a str,
@@ -24,60 +12,58 @@ struct Blocklist<'a> {
 }
 
 impl<'a> Blocklist<'a> {
+    async fn fetch(&self) -> Vec<u8> {
+        surf::get(self.uri()).recv_bytes().await.unwrap()
+    }
+
     fn uri(&self) -> String {
         format!("https://list.iblocklist.com/?list={}&fileformat=p2p&archiveformat=gz", self.id)
     }
-
-    fn download(&self) -> Result<File> {
-        eprintln!("Downloading list: {}", self.name);
-        let body = reqwest::blocking::get(&self.uri())?.bytes()?;
-        let mut dest = NamedTempFile::new()?;
-        dest.write_all(&body)?;
-        let reader = dest.reopen()?;
-        Ok(reader)
-    }
-}
-
-fn decompress(f: File) -> Result<File> {
-    let mut gz = GzDecoder::new(f);
-    let mut out = NamedTempFile::new()?;
-    io::copy(&mut gz, &mut out)?;
-    let reader = out.reopen()?;
-    Ok(reader)
-}
-
-fn valid_entries(f: File) -> Result<Vec<Result<String, std::io::Error>>> {
-    let empty_line_or_comment: Regex = Regex::new(r"(^$|^#.*$)")?;
-    let r = BufReader::new(f);
-    let valid_entries = r.lines()
-        .filter(|l| !empty_line_or_comment.is_match(l.as_ref().unwrap()))
-        .collect();
-    Ok(valid_entries)
-}
-
-fn get_output(output_opt: Option<PathBuf>) -> Box<dyn Write> {
-    output_opt
-        .map(|o| File::create(o.as_path()).expect("Invalid output path"))
-        .map(|f| Box::new(f) as Box<dyn Write>)
-        .unwrap_or(Box::new(std::io::stdout()) as Box<dyn Write>)
 }
 
 fn main() {
-    let opt = Opt::from_args();
-    let mut output = get_output(opt.output_file);
-
     let blocklists = vec![
         Blocklist { name: "level1", id: "ydxerpxkpcfqjaybcssw" },
         Blocklist { name: "level2", id: "gyisgnzbhppbvsphucsw" },
         Blocklist { name: "level3", id: "uwnukjqktoggdknzrhgh" },
     ];
 
-    blocklists.iter()
-        .map(|b| b.download().expect("Problem downloading blocklist"))
-        .map(|f| decompress(f).expect("Problem decompressing blocklist"))
-        .flat_map(|f| valid_entries(f).expect("Problem filtering invalid entries"))
-        .for_each(|l| {
-            writeln!(output, "{}", l.as_ref().unwrap())
-                .expect("Problem writing entry to output file");
-        })
+    let mut tasks = Vec::with_capacity(blocklists.len());
+    let raw_contents = Arc::new(Mutex::new(Vec::with_capacity(blocklists.len())));
+
+    for blocklist in blocklists.into_iter() {
+        let rc = Arc::clone(&raw_contents);
+        tasks.push(
+            task::spawn(
+                async move {
+                    rc.lock().await.push(blocklist.fetch().await)
+                }
+            )
+        )
+    };
+
+    task::block_on(async {
+        for t in tasks {
+            t.await;
+        }
+    });
+
+    let empty_line_or_comment: Regex = Regex::new(r"(^$|^#.*$)").unwrap();
+    let mut combined_contents = String::new();
+    let raw_contents = task::block_on(async { raw_contents.lock().await });
+    raw_contents.iter()
+        .map(|bytes| GzDecoder::new(&bytes[..])
+            .read_to_string(&mut combined_contents)
+            .unwrap()
+        )
+        .for_each(drop);
+
+    let final_list: Vec<&str> = combined_contents
+        .lines()
+        .filter(|l| !empty_line_or_comment.is_match(l))
+        .collect();
+
+    for entry in final_list {
+        writeln!(String::from(entry)).unwrap()
+    }
 }
